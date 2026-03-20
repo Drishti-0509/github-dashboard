c5657529219db0066ac62379af6f0cb711f3ee8f
Drishti-0509 <drishti.sharma0509@gmail.com>
2026-03-20 14:00:06 +0530

    Fix port for deployment

--- server/server.js
+++ server/server.js
@@ -21,8 +21,8 @@ app.use('/api/github', require('./routes/github'))
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('MongoDB connected!')
-    app.listen(5001, () => {
-      console.log('Server running on port 5001')
-    })
+    app.listen(process.env.PORT || 5001, () => {
+  console.log(`Server running on port ${process.env.PORT || 5001}`)
+})
   })
   .catch(err => console.log('Error:', err))
